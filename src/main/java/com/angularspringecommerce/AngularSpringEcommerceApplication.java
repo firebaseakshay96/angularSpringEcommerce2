c8a875b100f1dc94021f8af9a546b5992f49f469
Akshay Gayale <akshaygayale@gmail.com>
2021-09-17 20:59:57 +0530

    pushing the base project

---
+++ src/main/java/com/angularspringecommerce/AngularSpringEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.angularspringecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angularspringecommerce/AngularSpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularspringecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
